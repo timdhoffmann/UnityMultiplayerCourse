553b5f30208404985bf805f10b1ae5b6e8bb614e
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2020-11-27 20:16:22 +0100

    L14: Unit Movement.

---
+++ Assets/_Project/Player/Mover.cs
@@ -0,0 +1,66 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+namespace _Project.Player
+{
+    public class Mover : NetworkBehaviour
+    {
+        #region Fields
+
+        private NavMeshAgent _navMeshAgent = null;
+        private Camera _mainCamera = null;
+
+        #endregion
+
+        #region Server Methods
+
+        [Command]
+        private void CmdMoveToPosition(Vector3 position)
+        {
+            if (!NavMesh.SamplePosition(
+                sourcePosition: position,
+                hit: out NavMeshHit hit,
+                maxDistance: 1.0f,
+                areaMask: NavMesh.AllAreas))
+            {
+                return;
+            }
+            _navMeshAgent.SetDestination(hit.position);
+        }
+
+        #endregion
+
+        #region Client Methods
+
+        public override void OnStartAuthority()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            Assert.IsNotNull(_navMeshAgent);
+
+            _mainCamera = Camera.main;
+            Assert.IsNotNull(_mainCamera);
+        }
+
+        [ClientCallback]
+        private void Update()
+        {
+            if (!hasAuthority) { return; }
+            if (!Input.GetMouseButtonDown(1)) { return; }
+
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (!Physics.Raycast(
+                ray: ray,
+                hitInfo: out RaycastHit hitInfo,
+                maxDistance: Mathf.Infinity
+                ))
+            { return; }
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red, 1.0f);
+
+            CmdMoveToPosition(hitInfo.point);
+        }
+
+        #endregion
+    }
+}
