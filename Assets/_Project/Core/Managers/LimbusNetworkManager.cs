174086b6c33fb21ca753e9f80e6682821b478e01
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2020-11-29 20:47:07 +0100

    L15: Spawning Units.

--- Assets/_Project/Core/Managers/LimbusNetworkManager.cs
+++ Assets/_Project/Core/Managers/LimbusNetworkManager.cs
@@ -2,7 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class LimbusNetworkManager : NetworkManager
 {
+    [SerializeField]
+    private GameObject _unitSpawnerPrefab = null;
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+        GameObject unitSpawner = Instantiate(
+            _unitSpawnerPrefab,
+            conn.identity.transform.position,
+            conn.identity.transform.rotation);
+        NetworkServer.Spawn(unitSpawner, conn);
+    }
+
+    private void Start()
+    {
+        Assert.IsNotNull(_unitSpawnerPrefab);
+    }
 }
---
+++ Assets/_Project/Core/Managers/UnitSpawner.cs
@@ -0,0 +1,43 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+public class UnitSpawner : NetworkBehaviour
+{
+    [SerializeField]
+    private GameObject _unitPrefab = null;
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unit = Instantiate(_unitPrefab, Vector3.zero, Quaternion.identity);
+        NetworkServer.Spawn(unit, connectionToClient);
+        Debug.Log($"Spawned unit for client: {connectionToClient.connectionId.ToString()}");
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        Assert.IsNotNull(_unitPrefab);
+    }
+
+    [ClientCallback]
+    private void Update()
+    {
+        if (!hasAuthority) { return; }
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            CmdSpawnUnit();
+        }
+    }
+
+    #endregion
+}
