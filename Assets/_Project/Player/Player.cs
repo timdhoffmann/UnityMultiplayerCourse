8e828b47cba9c86870420fdc45fc7948a2c77ce0
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2020-12-07 18:05:43 +0100

    L18: Storing Our Units.

---
+++ Assets/_Project/Player/Player.cs
@@ -0,0 +1,88 @@
+﻿using _Project.Units;
+using Mirror;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace _Project.Player
+{
+    public class Player : NetworkBehaviour
+    {
+        [SerializeField] private List<Unit> _ownedUnits = new List<Unit>();
+
+        #region Server Methods
+
+        public override void OnStartServer()
+        {
+            /// Event subscribtions.
+            Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+            Unit.ServerOnUnitDeSpawned += ServerHandleUnitDeSpawned;
+        }
+
+        public override void OnStopServer()
+        {
+            /// Event un-subscriptions.
+            Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+            Unit.ServerOnUnitDeSpawned -= ServerHandleUnitDeSpawned;
+        }
+
+        private void ServerHandleUnitSpawned(Unit unit)
+        {
+            // Checks if the unit is this player's unit (are the unit and this player owned by
+            // the same connection?).
+            if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+            _ownedUnits.Add(unit);
+        }
+
+        private void ServerHandleUnitDeSpawned(Unit unit)
+        {
+            // Checks if the unit is this player's unit (are the unit and this player owned by
+            // the same connection?).
+            if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+            _ownedUnits.Remove(unit);
+        }
+
+        #endregion
+
+        #region Client Methods
+
+        public override void OnStartClient()
+        {
+            // Returns if we are the server.
+            if (!isClientOnly) { return; }
+
+            /// Event subscriptions.
+            Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+            Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitDeSpawned;
+        }
+
+        public override void OnStopClient()
+        {
+            // Returns if we are the server.
+            if (!isClientOnly) { return; }
+
+            /// Event un-subscriptions.
+            Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+            Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitDeSpawned;
+        }
+
+        private void AuthorityHandleUnitSpawned(Unit unit)
+        {
+            if (!hasAuthority) { return; }
+
+            _ownedUnits.Add(unit);
+        }
+
+        private void AuthorityHandleUnitDeSpawned(Unit unit)
+        {
+            if (!hasAuthority) { return; }
+
+            _ownedUnits.Remove(unit);
+        }
+
+        #endregion
+    }
+}
--- Assets/_Project/Units/Unit.cs
+++ Assets/_Project/Units/Unit.cs
@@ -1,4 +1,5 @@
 ﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,49 @@ namespace _Project.Units
 {
     public class Unit : NetworkBehaviour
     {
+        #region Events
+
+        public static event Action<Unit> AuthorityOnUnitSpawned;
+        public static event Action<Unit> AuthorityOnUnitDeSpawned;
+        public static event Action<Unit> ServerOnUnitSpawned;
+        public static event Action<Unit> ServerOnUnitDeSpawned;
+
+        #endregion
+
         [SerializeField]
         private UnityEvent _onSelected = null;
-
         [SerializeField]
         private UnityEvent _onDeselected = null;
 
+        #region Server Methods
+
+        public override void OnStartServer()
+        {
+            ServerOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopServer()
+        {
+            ServerOnUnitDeSpawned?.Invoke(this);
+        }
+
+        #endregion
+
         #region Client Methods
+
+        public override void OnStartClient()
+        {
+            // Returns if we are the server or don't have authority.
+            if (!isClientOnly || !hasAuthority) { return; }
+            AuthorityOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopClient()
+        {
+            // Returns if we are the server or don't have authority.
+            if (!isClientOnly || !hasAuthority) { return; }
+            AuthorityOnUnitDeSpawned?.Invoke(this);
+        }
 
         [Client]
         public void Select()
